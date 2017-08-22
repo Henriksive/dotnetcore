17182bae9ffd17a480d8f6a81051bc78e7509325
Henrik Sivertsgård <henrik.sivertsgard@bekk.no>
2017-08-22 16:49:50 +0200

    Add pingcontroller

---
+++ Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore.Controllers
+{
+    [Route("api/[controller]")]
+    public class PingController : Controller
+    {
+        // Ping
+        [HttpGet]
+        public string Pong()
+        {
+            return "Pong";
+        }
+    }
+}
